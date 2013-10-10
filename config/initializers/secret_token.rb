4b496842184efc574bb26e9b608b3745b446f32a
David Gardner <dgardner88@gmail.com>
2013-10-09 21:53:37 -0700

    Initializing repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EclipseBattleSim::Application.config.secret_key_base = '8b7626de0b4def9833cda35b567a1664cdac31145e512c416f705b7f61158d90da9002d6623aaa82e4665d748ed70195094fd000cd6dd2dc26bfd109574f0df7'
